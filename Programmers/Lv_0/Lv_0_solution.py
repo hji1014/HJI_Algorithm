f07207fd0a2e10e9d1a5daab830d76d823bd69bc
Junil Huh <hji10141@naver.com>
2023-06-04 16:24:39 +0900

    Update Lv_0_solution.py

--- Programmers/Lv_0/Lv_0_solution.py
+++ Programmers/Lv_0/Lv_0_solution.py
@@ -29,3 +29,32 @@ def solution(age):
 # 두 수의 합
 def solution(num1, num2):
     return num1 + num2
+
+# 두 수의 나눗셈
+def solution(num1, num2):
+    return int(1000 * (num1 / num2))
+
+# 각도기
+def solution(angle):
+    answer = 0
+    if 0 < angle < 90:
+        answer = 1
+    elif angle == 90:
+        answer = 2
+    elif 90< angle < 180:
+        answer = 3
+    elif angle == 180:
+        answer = 4
+    else:
+        pass
+    return answer
+
+# 짝수의 합
+def solution(n):
+    answer = 0
+    for i in range(2, n + 1):
+        if i % 2 == 0:
+            answer += i
+        else:
+            pass
+    return answer
