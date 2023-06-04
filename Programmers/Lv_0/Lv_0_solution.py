738dc72dd3cb0433d7bbe3ba4384cbb205d1ed00
Junil Huh <hji10141@naver.com>
2023-06-04 16:19:25 +0900

    Add files via upload

---
+++ Programmers/Lv_0/Lv_0_solution.py
@@ -0,0 +1,31 @@
+# 두 수의 차
+def solution(num1, num2):
+    return num1 - num2
+
+# 몫 구하기
+def solution(num1, num2):
+    return num1 // num2
+
+# 나머지 구하기
+def solution(num1, num2):
+    return num1 % num2
+
+# 두 수의 곱
+def solution(num1, num2):
+    return num1 * num2
+
+# 숫자 비교하기
+def solution(num1, num2):
+    if num1 == num2:
+        return 1
+    else:
+        return -1
+
+# 나이 출력
+def solution(age):
+    birth = 2023 - age
+    return birth
+
+# 두 수의 합
+def solution(num1, num2):
+    return num1 + num2
